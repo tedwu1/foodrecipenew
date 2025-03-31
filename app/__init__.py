bc7ef4948da5fd2f4aa972695399cbadec9b4b7f
Ted Wu <theodore.wu@sjsu.edu>
2025-03-31 00:19:48 -0700

    Your commit message here

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+# this is the user_loader function
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  # assuming you have a User model
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,19 @@
-from app import app, db
 from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user  # Add login_required here
+from app import app, db
 from app.forms import LoginForm, RecipeForm
 from app.models import User, Recipe
-from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
-@app.route("/")
-@app.route("/recipes")
-def home():
-    recipes = Recipe.query.all()
-    return render_template("recipes.html", recipes=recipes)
-
-@app.route("/recipe/new", methods=['GET', 'POST'])
-@login_required
-def new_recipe():
-    form = RecipeForm()
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
     if form.validate_on_submit():
-        recipe = Recipe(
-            title=form.title.data,
-            description=form.description.data,
-            ingredients=form.ingredients.data,
-            instructions=form.instructions.data,
-            author=current_user
-        )
-        db.session.add(recipe)
-        db.session.commit()
-        flash("Recipe added successfully!", "success")
-        return redirect(url_for('home'))
-    return render_template("new_recipe.html", form=form)
-
-@app.route("/recipe/<int:recipe_id>")
-def view_recipe(recipe_id):
-    recipe = Recipe.query.get_or_404(recipe_id)
-    return render_template("view_recipe.html", recipe=recipe)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("Login successful!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template('login.html', form=form)
